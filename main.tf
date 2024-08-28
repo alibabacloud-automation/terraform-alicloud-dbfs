9b934e4d2ad1864c1ab164fdeb1a5ac92945069b
shanye <shanye.xzq@alibaba-inc.com>
2024-08-28 14:34:27 +0800

    module/alicloud_dbfs_instance: update fs_name; example/complete: improve the examples;

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "alicloud_dbfs_service_linked_role" "service_linked_role" {
 
 resource "alicloud_dbfs_instance" "instance" {
   count             = var.create_instance ? 1 : 0
-  instance_name     = var.instance_name
+  fs_name           = var.instance_name
   category          = var.category
   zone_id           = var.zone_id
   performance_level = var.performance_level
